be10eb59f1f91d6cef2748e63c8d0b08f50f2348
Brian Denmark <testingarea479@gmail.com>
2023-02-14 21:03:01 -0800

    init commit

---
+++ src/main/java/com/dumpster/DumpsterApplication.java
@@ -0,0 +1,13 @@
+package com.dumpster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumpsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumpsterApplication.class, args);
+	}
+
+}
